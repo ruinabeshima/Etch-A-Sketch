5814250d41a1c2c92ea3bcfe51e41bcc3d5f7d98
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-09-13 22:28:55 +0900

    Added JS functionality and made box creator

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
-function createGridLines(){
-  const flexContainer = document.getElementById("flex-container")
-  for (i=0; i<16; i++){
+function createGridLines(num){
+  for (i=0; i<num; i++){
     let newRow = document.createElement("div")
     newRow.classList.add("flexRow")
     flexContainer.appendChild(newRow)
   }
 }
 
-function createGridBoxes(){
-  const flexRow = document.getElementsByClassName("flexRow")
+function createGridBoxes(num){
   for (const row of flexRow){
-    for (i=0; i<16; i++){
+    for (i=0; i<num; i++){
     let newDiv = document.createElement("div")
+    newDiv.classList.add("gridbox")
     row.append(newDiv)
     }
   }
 }
 
-createGridLines()
-createGridBoxes()
+function fillColour(){
+  for (const box of GridBox){
+    box.addEventListener("mouseover", function(){
+    box.classList.add("coloured")
+  })
+  }
+}
+
+function createNewGrid() {
+  createGrid.addEventListener("click", function(){
+  let inputValue = promptCreate.value
+  if (inputValue <= 100){
+    flexContainer.innerHTML = ""
+    createGridLines(inputValue)
+    createGridBoxes(inputValue)
+    fillColour()
+  } else {
+    alert ("Number must be less than 100!")
+  }
+  })
+}
+
+const flexContainer = document.getElementById("flex-container")
+const flexRow = document.getElementsByClassName("flexRow")
+const GridBox = document.getElementsByClassName("gridbox")
+const promptCreate = document.getElementById("prompt-create")
+const createGrid = document.getElementById("create-grid")
+
+createGridLines(16)
+createGridBoxes(16)
+fillColour()
+createNewGrid()
+
+
 
 
 
